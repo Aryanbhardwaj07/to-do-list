6e382067e8e6c17644b68600c8bd392ddbbfb4df
Aryanbhardwaj07 <firstnamearyanbhardwaj16@gmail.com>
2023-09-24 04:26:36 +0530

    Update Delete Button

--- app/page.js
+++ app/page.js
@@ -13,17 +13,21 @@ const page = () => {
     console.log(maintask)
   }
   let renderTask=<h2>No Task Avilable</h2>
+  if(maintask.length>0){
 
-  renderTask=maintask.map((t,i)=>{
-    return(
-    <li>
-      <div className='flex justify-between mb-5'>
+    
+    renderTask=maintask.map((t,i)=>{
+      return(
+      <li className='flex items-center justify-between mb-5'>
+      <div className='flex items-center w-2/3 justify-between mb-5'>
       <h5 className='text-2xl font-semibold'>{t.title}</h5>
       <h6 className='text-2xl font-semibold'>{t.desc}</h6>
       </div>
+      <button className='bg-red-400 mb-5 text-white rounded px-4 py-2 font-bold'>Delete</button>
     </li>
     )
   })
+}
   return (
      <>
       <h1 className='bg-black text-2xl font-bold text-center text-yellow-500 p-5'>Aryan TODO List</h1>
